32e06e1fa8bcb8b770134082e5f204412f36dd86
Najlaruiz <najla.badraouy@gmail.com>
2023-10-22 16:27:38 +0000

    Starbucks website using html css and js

---
+++ script.js
@@ -0,0 +1,5 @@
+function imgSlider(src, color) {
+    document.querySelector(`.starbucks`).src = src;
+    const circle = document.querySelector(".circle");
+    circle.style.backgroundColor = color;
+}
